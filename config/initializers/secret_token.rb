0fde7d99bf7d0f2cf2dd7e19940b26f22ffbe053
Emanuele Tozzato <etozzato@trueinteractive.net>
2011-01-02 12:49:26 -0800

    rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyesworldHtml::Application.config.secret_token = '2fd95dbd994f0430a02f317eb032a69e537c51c2c7db6b63caba0200ba6b59be06b73e9316e971dd5c341b818f9923eebb67f16bcfd5eea17fd2e33196d81fad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EyesworldHtml::Application.config.session_store :cookie_store, :key => '_eyesworld-html_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EyesworldHtml::Application.config.session_store :active_record_store
