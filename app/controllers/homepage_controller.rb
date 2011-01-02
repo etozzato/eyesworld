806cd20dd8a3f3d57b9465ea3c00ed0a9799a1bc
Emanuele Tozzato <etozzato@trueinteractive.net>
2011-01-02 14:33:09 -0500

    app stop

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,8 @@
 class HomepageController < ApplicationController
   
   def index
+    render :text => "L'Applicazione &egrave; in manutenzione! Torner&agrave; in funzione appena sar&agrave; completato
+    l'aggiornamento dei database per controllo di integrit&agrave; dovuto al passaggio al nuovo anno."
     @prima_pagina ||= RssReader.parseFeed('http://news.google.it/news?pz=1&hdlOnly=1&cf=all&ned=it&hl=it&topic=n&output=rss')
     @sport ||= RssReader.parseFeed('http://news.google.it/news?pz=1&hdlOnly=1&cf=all&ned=it&hl=it&topic=s&output=rss')
     @spettacolo ||= RssReader.parseFeed('http://news.google.it/news?pz=1&hdlOnly=1&cf=all&ned=it&hl=it&topic=e&output=rss')
