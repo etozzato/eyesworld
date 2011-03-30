0f3035b567d3de747cfd1dcfee3a7ca13a6c5f61
Emanuele Tozzato <etozzato@trueinteractive.net>
2011-03-30 00:10:01 -0700

    allright V5 ?

--- config/initializers/ar_hash_all.rb
+++ config/initializers/ar_hash_all.rb
@@ -2,9 +2,14 @@ class ActiveRecord::Base
 
   def self.hash_all(hash_key, *args)
     hash = {}
+    ar_column = args[0].delete(:ar_column)
     collection = find(:all, *args)
     unless collection.empty?
-      collection.each_with_index {|el, idx| hash[el.send(hash_key) || idx] = el}
+      if ar_column
+        collection.each_with_index {|el, idx| hash[el.send(hash_key) || idx] = el.send(ar_column)}
+      else
+        collection.each_with_index {|el, idx| hash[el.send(hash_key) || idx] = el}
+      end
     end
     hash
   end
